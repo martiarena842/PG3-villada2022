5e563dd73eea1215f2fbcc4e73ce56dab8e14bf8
martiarena842 <tomimartiarena@gmail.com>
2022-04-22 10:13:16 -0300

    Intentando desafio1 a carpeta

--- Guia1/desafio1.py
+++ Guia1/desafio1.py
@@ -1,7 +1,5 @@
-# def triangulo(A,ancho):
 
-# for i in range(ancho):
-#  print(ancho*A)
+
 
 
 from ntpath import join
@@ -11,24 +9,30 @@ ancho = int(input("ingresa el alto: "))
 A = str(input("Ingresa caracter: "))
 prim = ancho + 1
 first = " " * prim
-contador = 0
+juan=0
 
 
-print("" + first + A)
 if ancho > 1:
+    print("" + first + A)
     contador = 1
     while contador < ancho:
         k = ancho - contador
         print(" " * k, contador * A + " ".join(A) + contador * A)
         contador = contador + 1
 else:
-
-    while contador != abs(ancho):
-
-        k = ancho + contador
-        print(" " * k, contador * A + " ".join(A) + contador * A)
-        contador = contador + 1
-
-
+    contador=0
+    contador=abs(ancho)
+    while juan<10:
+        
+        contador=contador-1
+        k=contador-1
+        print(+contador*A+" "*k)
+        
+        juan=juan+1
+print("probando")
 # " "+A*contador
 # contador=alto +1
+  
+        #k = ancho + contador
+        #print(" " * k, contador * A + " ".join(A) + contador * A)
+        #contador = contador + 1
