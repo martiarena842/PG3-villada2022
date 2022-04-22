307c26e6f2607c56f9315192c039500814f82225
martiarena842 <tomimartiarena@gmail.com>
2022-04-22 08:16:27 -0300

    primera version

---
+++ desafio1.py
@@ -0,0 +1,34 @@
+# def triangulo(A,ancho):
+
+# for i in range(ancho):
+#  print(ancho*A)
+
+
+from ntpath import join
+
+
+ancho = int(input("ingresa el alto: "))
+A = str(input("Ingresa caracter: "))
+prim = ancho + 1
+first = " " * prim
+contador=0
+
+
+print("" + first + A)
+if ancho > 1:
+    contador = 1
+    while contador < ancho:
+        k = ancho - contador
+        print(" " * k, contador * A + " ".join(A) + contador * A)
+        contador = contador + 1
+else:
+            
+    while contador != abs(ancho):
+        
+        k = ancho + contador
+        print(" " * k, contador * A + " ".join(A) + contador * A)
+        contador = contador + 1
+        
+
+# " "+A*contador
+#contador=alto +1
