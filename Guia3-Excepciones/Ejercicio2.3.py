f4a09cd67af2fb6fcf2f24fa7c1fcd14e9a9b9c4
martiarena842 <tomimartiarena@gmail.com>
2022-05-26 08:12:11 -0300

    ejercicio2

--- Guia3-Excepciones/Ejercicio2.3.py
+++ Guia3-Excepciones/Ejercicio2.3.py
@@ -1,1 +1,12 @@
-print("ejercicio2")
+
+
+try:
+    num1=int(input("ingrese un numero: "))
+    num2=int(input("ingrese un numero: "))
+    div=num1/num2
+
+    print("La division da= ",div)
+
+except ZeroDivisionError:
+    print("no se puede dividir por cero, ingrese un numero valido. ")
+    pass
